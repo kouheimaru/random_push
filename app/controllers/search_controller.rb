463562176685537be1c6760ea7efab8c10ee9b8d
vagrant <vagrant@vagrant.vm>
2022-02-03 16:01:11 +0900

    set topic model

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
   def search
-    array = ["中居", "木村", "稲垣", "草彅", "香取"]
-    @theme = array.shuffle.slice(0,1)
+    array = Topic.select("text")
+    ans = array.shuffle.slice(0,1)
+    @theme =ans 
   end
 end
